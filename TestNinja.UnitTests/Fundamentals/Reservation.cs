b6a94a1a50dea3ba9182d7cce932e81f629b70c1
yachnytskyi <yachnytskyi1992@gmail.com>
2019-03-20 23:28:01 -0700

    Add Fundamentals/Reservation and ReservationTests, delete UnitTest1

---
+++ TestNinja.UnitTests/Fundamentals/Reservation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCandelledBy(User user)
+        {
+            if (user.IsAdmin)
+            {
+                return true;
+            }
+
+            if (MadeBy == user)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.UnitTests.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCandelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCandelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_Return_False()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCandelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+    }
+}
